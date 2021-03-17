50faa2015431c6ae993c4ea7c1801c50c8e5fc47
akil marshall <akilmarshall@gmail.com>
2021-03-16 16:29:27 -1000

    began reimplementing the api using FastApi

--- serve.py
+++ serve.py
@@ -1,50 +1,25 @@
-#!/usr/bin/python3
-from flask import Flask, jsonify
-from flask import render_template
-import json
+from fastapi import FastAPI
+import sqlalchemy as db
+import weatherpi
 
-import weatherpi.database as db
-
-
-app = Flask(__name__)
-application = app
-
-
+'''
 @app.route("/all_temperatures", methods=['GET'])
-def all_temperatures():
-    data = db.get_all_temperatures() 
-    return jsonify(data)
-
-
 @app.route("/all_humidities", methods=['GET'])
-def all_humidities():
-    data = db.get_all_humidities() 
-    return jsonify(data)
-
-
 @app.route("/all_pressures", methods=['GET'])
-def all_pressures():
-    data = db.get_all_pressures() 
-    return jsonify(data)
-
-
 @app.route("/all_pm10", methods=['GET'])
-def all_pm10():
-    data = db.get_all_pm10() 
-    return jsonify(data)
-
-
 @app.route("/all_pm25", methods=['GET'])
-def all_pm25():
-    data = db.get_all_pm25() 
-    return jsonify(data)
-
-
 @app.route("/all_pm100", methods=['GET'])
-def all_pm100():
-    data = db.get_all_pm100() 
-    return jsonify(data)
+'''
+
+app = FastAPI()
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# @app.get('/')
+# def read_root():
+#     return {'key': 'value'}
+
+@app.get('/all_temperatures')
+def get_all_temps():
+    '''
+    Returns all recorded temperatures as a list of (datetime, float[C]) tuples.'''
+    return {'all temperatures': weatherpi.get_all_temperatures()}
